6d3ad91935faef66e4db962c4d19cafc4c58183b
+ <+>
2022-02-24 10:00:18 -0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
+const { expect } = require("chai");
+
 // Code your solution in this file!
+function distanceFromHqInBlocks(blocknumber) {
+    if (blocknumber > 42)
+        return blocknumber - 42
+    else return 42 - blocknumber;
+}
+function distanceFromHqInFeet(blocknumber) {
+    return distanceFromHqInBlocks(blocknumber) * 264
+}
+function distanceTravelledInFeet(start, destination) {
+    const blockLength = 264;
+    if (start < destination)
+        return (destination - start) * blockLength;
+    else return (start - destination) * blockLength;
+}
+console.log(distanceTravelledInFeet(43, 48))
+
+
+
+   function calculatesFarePrice(start, destination) {
+        const distance = distanceTravelledInFeet(start, destination);
+
+        if (distance <= 400) {
+            return 0;
+        } else if (distance > 400 && distance <= 2000) {
+            return .02 * (distance - 400);
+        } else if (distance > 2000 && distance < 2500) {
+            return 25;
+        } else {
+            return 'cannot travel that far';
+        }
+    }
